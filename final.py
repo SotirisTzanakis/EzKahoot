1138fed6579bd45419c145f901a93e9c98a94088
Sotiral <rinaroze449@gmail.com>
2024-11-09 11:34:33 +0200

    final.py

--- final.py
+++ final.py
@@ -8,7 +8,7 @@ screen_width, screen_height = pyautogui.size()  # Get screen resolution
 center_x, center_y = screen_width // 2, screen_height // 2  # Calculate center coordinates
 
 # Move the mouse to the center of the screen
-pyautogui.moveTo(center_x, center_y, duration=1)
+pyautogui.moveTo(center_x, center_y, duration=0)
 
 #Triple-click to highlight the text
 pyautogui.click()  # First 
@@ -18,11 +18,11 @@ time.sleep(0.2)    # delay
 pyautogui.click()  # Third 
 
 #Copy the selected text (Ctrl+C)
-time.sleep(0.5)  #Wait bc why not
+time.sleep(0.3)  #Wait bc why not
 pyautogui.hotkey('ctrl', 'c')  # Press Ctrl+C to copy the selected text
 
 #Check if text is copied using pyperclip
-time.sleep(1)  # Wait for the clipboard to update
+time.sleep(0.8)  # Wait for the clipboard to update
 
 # Get the copied text from the clipboard
 copied_text = pyperclip.paste()
@@ -49,3 +49,27 @@ pyautogui.hotkey('ctrl', 'v')
 
 time.sleep(0.5)  #Wait bc why not
 pyautogui.hotkey('enter', 'enter')
+
+time.sleep(2)
+
+# Define the x and y coordinates
+x11 = 350  
+y11 = 500  
+
+# Move the mouse to the coordinates over a specified duration
+pyautogui.moveTo(x11, y11, duration=0.1)  # Moves
+
+# Click at the location
+pyautogui.click()
+
+time.sleep(1.5)
+
+# Define the x and y coordinates
+x111 = 1850  
+y111 = 245  
+
+# Move the mouse to the coordinates over a specified duration
+pyautogui.moveTo(x111, y111, duration=0.1)  # Moves
+
+# Click at the location
+pyautogui.click()
